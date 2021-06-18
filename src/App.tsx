4e1ceff453f11b60dc2a16ff05be55593bc408e8
yujo <bedro27@gmail.com>
2021-06-18 17:45:23 +0900

    feat: 기본 UI 작성

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,17 @@ const App = (): JSX.Element => {
         <button className="p-2 bg-red-300">글 등록하기</button>
       </form>
       <h2 className="text-xl font-semibold">글 목록</h2>
+      <div className="w-4/12">
+        <h3 className="text-xl font-medium mb-1">Ipsum Loren</h3>
+        <p className="overflow-hidden">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+          standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to
+          make a type specimen book. It has survived not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+          sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus
+          PageMaker including versions of Lorem Ipsum.
+        </p>
+      </div>
     </div>
   );
 };
